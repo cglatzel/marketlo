3d897d84b09118a2a5af94940d63e3758ad64e33
Christian Glatzel <christianglatzel@outlook.com>
2026-02-03 22:01:19 +0100

    initialize a Spring Boot application

---
+++ src/main/kotlin/com/example/marketlo/MarketloApplication.kt
@@ -0,0 +1,11 @@
+package com.example.marketlo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MarketloApplication
+
+fun main(args: Array<String>) {
+	runApplication<MarketloApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/marketlo/MarketloApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.marketlo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MarketloApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
