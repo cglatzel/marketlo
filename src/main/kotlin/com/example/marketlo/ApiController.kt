813885b45c63866290cdac359a4c27e3be3fce80
Christian Glatzel <christianglatzel@outlook.com>
2026-02-05 18:12:13 +0100

    add paths to the endpoints

--- src/main/kotlin/com/example/marketlo/ApiController.kt
+++ src/main/kotlin/com/example/marketlo/ApiController.kt
@@ -1,12 +1,16 @@
 package com.example.marketlo
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/api")
 class ApiController(
     private val productService: ProductService
 ) {
 
+    @GetMapping("/products")
     fun getProducts(): List<Product> {
         return productService.retrieveAllProducts();
     }
